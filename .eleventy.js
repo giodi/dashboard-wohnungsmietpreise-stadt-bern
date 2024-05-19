12ac60234fd60b0be90133e969103e5056660a70
Giò <mail@gionathandiani.name>
2024-05-20 00:24:25 +0200

    Added metadata.

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,17 @@ module.exports = function(eleventyConfig) {
 		return new CleanCSS({}).minify(code).styles;
 	});
 
+	eleventyConfig.addShortcode("datenow", () => {
+		const now = new Date();
+		const year = now.getFullYear();
+		let month = now.getMonth() + 1;
+		month = month < 10  ? '0'+month : month;
+		let day = now.getDay() + 1;
+		day = day < 10  ? '0'+day : day;
+
+		return `${year}-${month}-${day}`;
+	});
+
 	eleventyConfig.addPassthroughCopy("./src/assets/img");
 	eleventyConfig.addPassthroughCopy("./src/assets/js");
 
